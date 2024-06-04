bc41e25b7606c5c627c8817f1613716c5e25dd69
Joaquin Tissera <phoenix_css@hotmail.com>
2024-06-03 22:13:42 -0300

    Main: Fix peliculas.calificacion: now is a String to match Enum SQL Type. Add consulta2 and consulta3 at consultasPropias method & some utils prints and comments

--- JavaMySql/Main.java
+++ JavaMySql/Main.java
@@ -5,7 +5,6 @@ import java.sql.Date;
 import java.io.IOException;
 import java.util.Scanner;
 import java.util.Properties;
-import java.math.BigDecimal;
 
 import conexion.Conexion;
 
@@ -31,24 +30,31 @@ public class Main {
                 opcionSeleccionada(resEntry);
                 switch (resEntry) {
                     case 1:
+                        // Pedida para el archivo Java
                         insertarCine(conexion);
                         break;
                     case 2:
+                        // Pedida para el archivo Java
                         insertarSalaEnCine(conexion);
                         break;
                     case 3:
+                        // Pedida para el archivo Java
                         listarCinesInfoSalas(conexion);
                         break;
                     case 4:
+                        // Resolucion de consultas (No necesariamente Java)
                         actoresUnaPelícula(conexion);
                         break;
                     case 5:
+                        // Resolucion de consultas (No necesariamente Java)
                         listarActoresYDirectores(conexion);
                         break;
                     case 6:
+                        // Resolucion de consultas (No necesariamente Java)
                         listarCinesCantButacas(conexion);
                         break;
                     case 7:
+                        // Resolucion de consultas (No necesariamente Java)
                         consultasPropias(conexion);
                         break;
                 }
@@ -57,7 +63,10 @@ public class Main {
     }
 
     private static void mostrarMenu() {
+        System.out.println();
         System.out.println("# Menú principal #");
+        System.out.println("Las primeras tres opciones son pedidas para este archivo Java");
+        System.out.println("Mientras que las demas no necesariamente son para Java, sino para el proyecto en general");
         System.out.println("·     Opción 1: Insertar un cine");
         System.out.println("·     Opción 2: Insertar una sala en un cine");
         System.out.println("·     Opción 3: Listar todos los cines con la información de sus salas");
@@ -79,24 +88,31 @@ public class Main {
         switch (entry) {
             case 1:
                 System.out.println("· Opción 1: Insertar un cine");
+                System.out.println("Opcion pedida para este archivo Java");
                 break;
             case 2:
                 System.out.println("· Opción 2: Insertar una sala en un cine");
+                System.out.println("Opcion pedida para este archivo Java");
                 break;
             case 3:
                 System.out.println("· Opción 3: Listar todos los cines con la información de sus salas");
+                System.out.println("Opcion pedida para este archivo Java");
                 break;
             case 4:
                 System.out.println("· Opción 4: Devolver actores que solo figuran en una sola película");
+                System.out.println("Opcion no obligatoria para este archivo Java");
                 break;
             case 5:
                 System.out.println("· Opción 5: Listar las personas que han sido actores y directores");
+                System.out.println("Opcion no obligatoria para este archivo Java");
                 break;
             case 6:
                 System.out.println("· Opción 6: Listar los cines con la cantidad total de butacas totales");
+                System.out.println("Opcion no obligatoria para este archivo Java");
                 break;
             case 7:
                 System.out.println("· Opción 7: Resultado de consultas propias");
+                System.out.println("Opcion no obligatoria para este archivo Java");
                 break;
             default:
                 System.out.println("· Opción 8: Salir");
@@ -238,8 +254,10 @@ public class Main {
 
     private static void consultasPropias(Conexion conexion) {
         // Consultas adicionales
-        String consulta = "SELECT * FROM pelicula WHERE calificacion > 4.0";
-        try (PreparedStatement statement = conexion.prepareStatement(consulta)) {
+        // Inicio consulta1: Listar las peliculas que tienen calificacion APT
+        System.out.println("    Consulta1: Listar las peliculas que tienen calificacion Apta para todo Publico");
+        String consulta1 = "SELECT * FROM pelicula WHERE calificacion = 'APT'";
+        try (PreparedStatement statement = conexion.prepareStatement(consulta1)) {
             ResultSet resultSet = statement.executeQuery();
             while (resultSet.next()) {
                 int id = resultSet.getInt("id");
@@ -247,16 +265,80 @@ public class Main {
                 String idiomaOriginal = resultSet.getString("idioma_original");
                 String url = resultSet.getString("url");
                 String duracion = resultSet.getString("duracion");
-                BigDecimal calificacion = resultSet.getBigDecimal("calificacion");
+                String calificacion = resultSet.getString("calificacion");
                 Date fechaEstreno = resultSet.getDate("fecha_estreno_Argentina");
                 String resumen = resultSet.getString("resumen");
+                String tituloDistribucion = resultSet.getString("titulo_distribucion");
+                String tituloOriginal = resultSet.getString("titulo_original");
+                String tituloEspaniol = resultSet.getString("titulo_espaniol");
 
                 System.out.println("ID: " + id + ", Género: " + genero + ", Idioma Original: " + idiomaOriginal);
                 System.out.println("URL: " + url + ", Duración: " + duracion + ", Calificación: " + calificacion);
                 System.out.println("Fecha de Estreno en Argentina: " + fechaEstreno + ", Resumen: " + resumen);
+                System.out.println(
+                        "Título de Distribución: " + tituloDistribucion + ", Título Original: " + tituloOriginal);
+                System.out.println("Título en Español: " + tituloEspaniol);
             }
         } catch (SQLException e) {
+            System.out.println("consultasPropias: consulta1");
             System.out.println("Error al listar datos: " + e.getMessage());
         }
+        // Fin consulta1
+        // Inicio consulta2: Paises con la mayor produccion en peliculas
+        System.out.println("    Consulta2: Paises con la mayor produccion en peliculas");
+        String consulta2 = "SELECT nombre FROM pais WHERE (" +
+                "SELECT COUNT(*) FROM origen_produccion op WHERE op.nombre = pais.nombre GROUP BY op.id" +
+                ") = (" +
+                "    SELECT MAX(producciones) AS cantMaxPeliculas FROM (" +
+                "SELECT COUNT(*) AS producciones FROM origen_produccion GROUP BY nombre" +
+                ") AS subconsulta" +
+                ")";
+        /*
+         * Selecciono nombres de paises tales que:
+         * La cantidad de peliculas con origen nombre.pais
+         * (cantidad = count(*) , con origen WHERE op.nombre = pais.nombre)
+         * Es la maxima cantidad de peliculas producidas por ese pais
+         * (max(producciones) FROM (cantidad de producciones desde origen_produccion))
+         * Es decir, si la maxima cantidad de peliculas encontradas es 5
+         * (por un pais cualquiera), se van a listar todos aquellos paises
+         * tales que produjeron 5 peliculas.
+         */
+        try (PreparedStatement statement = conexion.prepareStatement(consulta2)) {
+            ResultSet resultSet = statement.executeQuery();
+            int nroPais = 0;
+            if (resultSet.next()) {
+                do {
+                    nroPais++;
+                    String nombrePais = resultSet.getString("nombre");
+                    System.out.println(nroPais + "- Nombre: " + nombrePais);
+                } while (resultSet.next());
+            } else {
+                System.out.println("No hay directores que hayan actuado!");
+            }
+        } catch (SQLException e) {
+            System.out.println("consultasPropias: consulta2");
+            System.out.println("Error al listar datos: " + e.getMessage());
+        }
+        // Fin consulta2
+        // Inicio consulta3: Películas con la mayor duración en cada género
+        System.out.println("    Consulta3: Películas con la mayor duración en cada género");
+        String consulta3 = "SELECT id, genero, titulo_original, duracion FROM pelicula p1 " +
+                "WHERE duracion = (SELECT MAX(duracion) FROM pelicula p2 WHERE p1.genero = p2.genero)";
+        try (PreparedStatement statement = conexion.prepareStatement(consulta3)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                int id = resultSet.getInt("id");
+                String genero = resultSet.getString("genero");
+                String tituloOriginal = resultSet.getString("titulo_original");
+                String duracion = resultSet.getString("duracion");
+
+                System.out.println("ID: " + id + ", Género: " + genero + ", Titulo Original: " + tituloOriginal
+                        + ", Duración: " + duracion);
+            }
+        } catch (SQLException e) {
+            System.out.println("consultasPropias: consulta3");
+            System.out.println("Error al listar datos: " + e.getMessage());
+        }
+        // Fin consulta3
     }
 }
